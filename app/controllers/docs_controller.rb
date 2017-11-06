2dfa07307b371ec2947a0f66f4be956e19c870e6
nanomanon56 <manon.l.jean@gmail.com>
2017-11-06 08:56:26 -0500

    updated create delete edit

--- app/controllers/docs_controller.rb
+++ app/controllers/docs_controller.rb
@@ -1,10 +1,12 @@
 class DocsController < ApplicationController
-	
+	before_action :find_doc, only: [:show, :edit, :update, :destroy]
+
 	def index
-		@doc =Doc.new
+       @docs = Doc.all.order("created at DESC")
 	end
 
 	def show
+		@doc =Doc.find(params[:id])
 	end
 
 	def new
@@ -21,15 +23,26 @@ class DocsController < ApplicationController
 		end
 	end
 
+	def edit
+	end	
+
 	def update
+		if @doc.update(doc_params)
+			redirect_to @doc
+		else
+			render 'edit'
+		end
 	end
 
 	def destory 
+		@doc.destory
+		redirect_to_docs_path
 	end
 
        private
 
        def find_doc
+       	@doc =Doc.find(params[:id])
        end
 
        def doc_params
